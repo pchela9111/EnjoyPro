66e03c7d5a6b162d56ed6c5bf5fb123ea88054aa
Elena <pchelena911@gmail.com>
2019-12-04 10:16:34 +0300

    Каталог, сортировка

--- source/js/script.js
+++ source/js/script.js
@@ -8,8 +8,9 @@ var navLogo = document.querySelector(".nav-main__image");
 navMain.classList.remove("nav-main--nojs");
 navLogo.src = "img/ejpro-logo-white.svg";
 
-navToggle.addEventListener("click", function() {
+navToggle.addEventListener("click", function(evt) {
   if (navMain.classList.contains("nav-main--closed")) {
+    evt.preventDefault();
     navMain.classList.remove("nav-main--closed");
     navMain.classList.add("nav-main--opened");
     navLogo.src = "img/ejpro-logo-green.svg";
@@ -26,6 +27,18 @@ if (document.querySelector('.catalog__filter-menu')) {
 
   sortingLink.addEventListener('click', function (a) {
     a.preventDefault();
+    console.log("Клик по ссылке вход");
     sortingPopup.classList.toggle('catalog__filter-menu--opened');
+  });
+}
+
+if (document.querySelector('.catalog__brands-menu')) {
+  var menuButton = document.querySelector('.catalog__filter-link--btn');
+  var menuPopup = document.querySelector('.catalog__brands-menu');
+
+  menuButton.addEventListener('click', function (a) {
+    a.preventDefault();
+    console.log("Клик по ссылке вход");
+    menuPopup.classList.toggle('catalog__brands-menu--opened');
   });
 }
